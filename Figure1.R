c4b44d73b94c7c224cecf184b9dadd3d3dd59cf7
ameliatorster-hub <amelia.torster@gmail.com>
2026-07-02 21:14:59 -0400

    Design tibble commit, information in place, now to get the figure in order.

---
+++ Figure1.R
@@ -0,0 +1,10 @@
+install.packages("DiagrammeR")
+library(DiagrammeR)
+
+install.packages("remotes") #installs github
+library(remotes)
+
+remotes::install_github("stopsack/khsmisc")
+
+library(khsmisc)
+library(stringr)
